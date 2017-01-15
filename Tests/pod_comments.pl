43e7b0fbda26e268231ba1b25850106353537a83
Michael Sheets <git@whitefalls.org>
2017-01-14 18:29:36 -0600

    Simple POD comment test file

---
+++ Tests/pod_comments.pl
@@ -0,0 +1,27 @@
+=pod
+See L<perlpod(1)>.
+
+Text I<< <italic> >> Text
+
+=begin html
+<b>Bold</b>
+=end html
+
+=cut
+
+# Cut embedded snippets off early before a terminating =cut command
+=pod
+=begin html
+<b>Bold</b>
+=cut
+my $var;
+
+__END__
+
+See L<perlpod(1)>.
+
+Text I<< <italic> >> Text
+
+=begin html
+<b>Bold</b>
+=end html
